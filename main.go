73f1f4f6ca64e619ad7294080baf9f59ef646a80
0xScraipa <denrodtabuzo90@gmail.com>
2024-09-16 13:52:25 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"math/rand"
 	"net/http"
+	"net/url"
+	"os"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/exp/rand"
 )
 
 var apiKeys = []string{
@@ -25,22 +28,59 @@ var apiKeys = []string{
 	"CG-MuKoMVL1JZ8Mv4BhcMYPEF6M",
 }
 
+type envConfig struct {
+	APIKey string `json:"api_key"`
+	RPC    string `json:"rpc"`
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano()) // Seed the random number generator
+
+	cfg := &envConfig{
+		APIKey: os.Getenv("UPSHOT_APIKEY"),
+		RPC:    os.Getenv("RPC"),
+	}
+
+	fmt.Println("UPSHOT_APIKEY: ", cfg.APIKey)
+	fmt.Println("RPC: ", cfg.RPC)
 
 	router := gin.Default()
 
 	router.GET("/inference/:token", func(c *gin.Context) {
 		token := c.Param("token")
-		price, err := getLastPrice(token)
+		if token == "MEME" {
+			handleMemeRequest(c, cfg)
+			return
+		}
+
+		price, err := getAdjustedPrice(token)
 		if err != nil {
 			c.String(http.StatusInternalServerError, "Error: %v", err)
 			return
 		}
+
 		c.String(http.StatusOK, price)
 	})
 
 	router.Run(":8000")
+}
+
+func getAdjustedPrice(token string) (string, error) {
+	price, err := getSimplePrice(token)
+	if err != nil {
+		return "", err
+	}
+
+	currentPrice, err := strconv.ParseFloat(price, 64)
+	if err != nil {
+		return "", err
+	}
+
+	// Randomly adjust price by ±0.8%
+	adjustmentFactor := rand.Float64()*0.016 + 0.992
+	adjustedPrice := currentPrice * adjustmentFactor
+
+	return fmt.Sprintf("%.2f", adjustedPrice), nil
 }
 
 func getSimplePrice(token string) (string, error) {
@@ -53,7 +93,6 @@ func getSimplePrice(token string) (string, error) {
 		"ARB": "arbitrum",
 	}
 
-	// Randomly select an API key
 	selectedKey := apiKeys[rand.Intn(len(apiKeys))]
 
 	headers := map[string]string{
@@ -102,20 +141,163 @@ func getSimplePrice(token string) (string, error) {
 	return fmt.Sprintf("%.2f", price), nil
 }
 
-func getLastPrice(token string) (string, error) {
-	currentPriceStr, err := getSimplePrice(token)
-	if err != nil {
-		return "", err
+func handleMemeRequest(c *gin.Context, cfg *envConfig) {
+	if cfg.APIKey == "" {
+		c.String(http.StatusBadRequest, "need API key")
+		return
 	}
 
-	currentPrice, err := strconv.ParseFloat(currentPriceStr, 64)
-	if err != nil {
-		return "", err
+	if cfg.RPC == "" {
+		c.String(http.StatusInternalServerError, "Invalid RPC URL")
+		return
 	}
 
-	// Randomly adjust price by ±0.8%
-	adjustmentFactor := rand.Float64()*0.016 + 0.992
-	adjustedPrice := currentPrice * adjustmentFactor
+	lb, err := getLatestBlock(cfg.RPC)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Error fetching latest block: %v", err)
+		return
+	}
 
-	return fmt.Sprintf("%.2f", adjustedPrice), nil
+	meme, err := getMemeOracleData(lb, cfg.APIKey)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Error fetching meme data: %v", err)
+		return
+	}
+
+	mp, err := getMemePrice(meme.Data.Platform, meme.Data.Address)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Error fetching meme price: %v", err)
+		return
+	}
+
+	mpf, _ := strconv.ParseFloat(mp, 64)
+	adjustedPrice := random(mpf)
+
+	c.String(http.StatusOK, fmt.Sprintf("%.6f", adjustedPrice))
+}
+
+func getMemePrice(network, memeAddress string) (string, error) {
+	url := fmt.Sprintf("https://api.geckoterminal.com/api/v2/simple/networks/%s/token_price/%s", network, memeAddress)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create new request: %w", err)
+	}
+	req.Header.Set("accept", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to execute request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	var res tokenPriceResponse
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return "", fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+
+	price, ok := res.Data.Attributes.TokenPrices[memeAddress]
+	if !ok {
+		return "", fmt.Errorf("price not found for address %s", memeAddress)
+	}
+
+	return price, nil
+}
+
+func getLatestBlock(rpc string) (string, error) {
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/status", rpc), nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create new request: %w", err)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to execute request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	var response latestBlockResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return "", fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+
+	return response.Result.SyncInfo.LatestBlockHeight, nil
+}
+
+func getMemeOracleData(blockHeight, apiKey string) (*memeOracleResponse, error) {
+	url := fmt.Sprintf("https://api.upshot.xyz/v2/allora/tokens-oracle/token/%s", blockHeight)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create new request: %w", err)
+	}
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("x-api-key", apiKey)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var res memeOracleResponse
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+
+	return &res, nil
+}
+
+func random(price float64) float64 {
+	randomPercent := rand.Float64()*6 - 3
+	priceChange := price * (randomPercent / 100)
+	return price + priceChange
+}
+
+type tokenPriceResponse struct {
+	Data struct {
+		Attributes struct {
+			TokenPrices map[string]string `json:"token_prices"`
+		} `json:"attributes"`
+	} `json:"data"`
+}
+
+type latestBlockResponse struct {
+	Result struct {
+		SyncInfo struct {
+			LatestBlockHeight string `json:"latest_block_height"`
+		} `json:"sync_info"`
+	} `json:"result"`
+}
+
+type memeOracleResponse struct {
+	RequestID string `json:"request_id"`
+	Status    bool   `json:"status"`
+	Data      struct {
+		TokenID     string `json:"token_id"`
+		TokenSymbol string `json:"token_symbol"`
+		Platform    string `json:"platform"`
+		Address     string `json:"address"`
+	} `json:"data"`
 }
